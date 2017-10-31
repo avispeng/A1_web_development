1fe648387b21418cb4ebaa0d00ee60fcd60a77f0
avispeng <hypeng1993@gmail.com>
2017-10-31 15:03:56 -0400

    fix bugs3

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 #!venv/bin/python
 from app import webapp
 # run local ONLY to avoid potential incoming traffic on my own machine
-webapp.run(host=0.0.0.0,port=80, debug=True)
+webapp.run(host='0.0.0.0',port=80, debug=True)
 #webapp.run(host='0.0.0.0')
