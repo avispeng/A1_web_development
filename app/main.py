b785d340daadb9f42720f81f7ccb5f207009a8e3
avispeng <hypeng1993@gmail.com>
2017-11-02 21:58:03 -0400

    fix things

--- app/main.py
+++ app/main.py
@@ -8,7 +8,8 @@ import os
 from wand.image import Image
 import boto3
 
-webapp.secret_key = os.urandom(24)
+# webapp.secret_key = os.urandom(24)
+webapp.secret_key = 'DJidfowieKD763EFJksjoefi'
 
 @webapp.route('/',methods=['GET'])
 @webapp.route('/index',methods=['GET'])
